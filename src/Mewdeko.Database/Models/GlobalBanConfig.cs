0cf4c58790279d0036d89f74afc574160afaa040
SylveonDeko <crimsonslayer76@gmail.com>
2023-09-30 22:03:35 -0400

    global ban api, other mewdeko improvements too!
    (cherry picked from commit 691a5769fb8e9bc46cdc541cfae3d5feb0f18264)

--- src/Mewdeko.Database/Models/GlobalBanConfig.cs
+++ src/Mewdeko.Database/Models/GlobalBanConfig.cs
@@ -3,7 +3,6 @@
 public class GlobalBanConfig : DbEntity
 {
     public ulong GuildId { get; set; }
-    public GbType BanTypes { get; set; }
     public ulong GlobalBanLogChannel { get; set; }
     public GBActionType Action { get; set; }
     public long UseRecommendedAction { get; set; }
