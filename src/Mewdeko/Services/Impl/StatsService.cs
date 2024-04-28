8de1664b9f7c2546d728758ec25c0bcf5f2849e1
sylveondeko <sylveon@mewdeko.tech>
2024-04-27 23:55:16 -0400

    yay more shit
    (cherry picked from commit a59d6e39baf38c75b1a17c1f65ec3fd4a100ae29)

--- src/Mewdeko/Services/Impl/StatsService.cs
+++ src/Mewdeko/Services/Impl/StatsService.cs
@@ -17,21 +17,19 @@ namespace Mewdeko.Services.Impl
     /// </summary>
     public class StatsService : IStatsService, IReadyExecutor, IDisposable
     {
-        private readonly DiscordSocketClient client;
-        private readonly IBotCredentials creds;
-        private readonly ICoordinator coord;
-        private readonly HttpClient http;
-        private readonly IDataCache cache;
-
         /// <summary>
         /// The version of the bot. I should make this set from commits somehow idk
         /// </summary>
         public const string BotVersion = "7.5.2";
 
+        private readonly IDataCache cache;
+        private readonly DiscordSocketClient client;
+        private readonly ICoordinator coord;
+        private readonly IBotCredentials creds;
+        private readonly HttpClient http;
+
         private readonly DateTime started;
-        private PeriodicTimer statcordTimer;
         private PeriodicTimer topGgTimer;
-        private PeriodicTimer updateGuildsTimer;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="StatsService"/> class.
@@ -56,13 +54,6 @@ namespace Mewdeko.Services.Impl
             started = DateTime.UtcNow;
 
             InitializePeriodicTasks(cmdServ);
-        }
-
-        private void InitializePeriodicTasks(CommandService cmdServ)
-        {
-            _ = PostToTopGg();
-            _ = PostToStatcord(coord, client, cmdServ);
-            _ = OnReadyAsync();
         }
 
         /// <summary>
@@ -112,83 +103,11 @@ namespace Mewdeko.Services.Impl
         }
 
         /// <summary>
-        /// Gets the memory usage of the bot.
+        /// Disposes of the timers.
         /// </summary>
-        public string Heap => ByteSize.FromBytes(Process.GetCurrentProcess().PrivateMemorySize64).Megabytes
-            .ToString(CultureInfo.InvariantCulture);
-
-        private TimeSpan GetUptime() => DateTime.UtcNow - started;
-
-        /// <summary>
-        /// Gets the uptime of the bot as a human-readable string.
-        /// </summary>
-        /// <param name="separator">The separator</param>
-        /// <returns>A string used in .stats to display uptime</returns>
-        public string GetUptimeString(string separator = ", ") =>
-            GetUptime().Humanize(2, minUnit: TimeUnit.Minute, collectionSeparator: separator);
-
-        private async Task PostToStatcord(ICoordinator coord, DiscordSocketClient socketClient, CommandService cmdServ)
+        public void Dispose()
         {
-            if (string.IsNullOrWhiteSpace(creds.StatcordKey)) return;
-
-            statcordTimer = new PeriodicTimer(TimeSpan.FromMinutes(1));
-
-            while (await statcordTimer.WaitForNextTickAsync().ConfigureAwait(false))
-            {
-                var memoryUsage = ByteSize.FromBytes(Process.GetCurrentProcess().PrivateMemorySize64).Bytes;
-                var content = Json.Serialize(new
-                {
-                    id = socketClient.CurrentUser.Id.ToString(),
-                    key = creds.StatcordKey,
-                    servers = coord.GetGuildCount().ToString(),
-                    users = coord.GetUserCount().ToString(),
-                    active = Array.Empty<string>(),
-                    commands = "0",
-                    popular = Array.Empty<string>(),
-                    memactive = memoryUsage.ToString(),
-                    memload = "0",
-                    cpuload = "0",
-                    bandwidth = "0",
-                    custom1 = cmdServ.Commands.Count().ToString()
-                });
-
-                var stringContent = new StringContent(content, System.Text.Encoding.UTF8, "application/json");
-                await http.PostAsync("https://api.statcord.com/beta/stats", stringContent).ConfigureAwait(false);
-            }
-        }
-
-        private async Task PostToTopGg()
-        {
-            if (client.ShardId != 0 || string.IsNullOrEmpty(creds.VotesToken)) return;
-
-            topGgTimer = new PeriodicTimer(TimeSpan.FromSeconds(10));
-
-            while (await topGgTimer.WaitForNextTickAsync().ConfigureAwait(false))
-            {
-                var content = new FormUrlEncodedContent(new Dictionary<string, string>
-                {
-                    {
-                        "shard_count", creds.TotalShards.ToString()
-                    },
-                    {
-                        "shard_id", client.ShardId.ToString()
-                    },
-                    {
-                        "server_count", coord.GetGuildCount().ToString()
-                    }
-                });
-
-                http.DefaultRequestHeaders.Authorization =
-                    new System.Net.Http.Headers.AuthenticationHeaderValue("Authorization", creds.VotesToken);
-                var response = await http
-                    .PostAsync(new Uri($"https://top.gg/api/bots/{client.CurrentUser.Id}/stats"), content)
-                    .ConfigureAwait(false);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    Log.Error("Failed to post stats to Top.gg");
-                }
-            }
+            topGgTimer.Dispose();
         }
 
         /// <inheritdoc/>
@@ -227,29 +146,76 @@ namespace Mewdeko.Services.Impl
         }
 
         /// <summary>
-        /// Disposes of the timers.
+        /// Gets the memory usage of the bot.
         /// </summary>
-        public void Dispose()
+        public string Heap => ByteSize.FromBytes(Process.GetCurrentProcess().PrivateMemorySize64).Megabytes
+            .ToString(CultureInfo.InvariantCulture);
+
+        /// <summary>
+        /// Gets the uptime of the bot as a human-readable string.
+        /// </summary>
+        /// <param name="separator">The separator</param>
+        /// <returns>A string used in .stats to display uptime</returns>
+        public string GetUptimeString(string separator = ", ") =>
+            GetUptime().Humanize(2, minUnit: TimeUnit.Minute, collectionSeparator: separator);
+
+        private void InitializePeriodicTasks(CommandService cmdServ)
         {
-            statcordTimer.Dispose();
-            topGgTimer.Dispose();
-            updateGuildsTimer.Dispose();
+            _ = PostToTopGg();
+            _ = OnReadyAsync();
+        }
+
+        private TimeSpan GetUptime() => DateTime.UtcNow - started;
+
+
+        private async Task PostToTopGg()
+        {
+            if (client.ShardId != 0 || string.IsNullOrEmpty(creds.VotesToken)) return;
+
+            topGgTimer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+
+            while (await topGgTimer.WaitForNextTickAsync().ConfigureAwait(false))
+            {
+                var content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {
+                        "shard_count", creds.TotalShards.ToString()
+                    },
+                    {
+                        "shard_id", client.ShardId.ToString()
+                    },
+                    {
+                        "server_count", coord.GetGuildCount().ToString()
+                    }
+                });
+
+                http.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Authorization", creds.VotesToken);
+                var response = await http
+                    .PostAsync(new Uri($"https://top.gg/api/bots/{client.CurrentUser.Id}/stats"), content)
+                    .ConfigureAwait(false);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    Log.Error("Failed to post stats to Top.gg");
+                }
+            }
         }
 
         /// <summary>
         /// Represents a partial guild information.
         /// </summary>
-        public class MewdekoPartialGuild
+        private class MewdekoPartialGuild
         {
             /// <summary>
             /// Gets or sets the name of the guild.
             /// </summary>
-            public string Name { get; set; }
+            public string? Name { get; set; }
 
             /// <summary>
             /// Gets or sets the URL of the guild's icon.
             /// </summary>
-            public string IconUrl { get; set; }
+            public string? IconUrl { get; set; }
 
             /// <summary>
             /// Gets or sets the number of members in the guild.
@@ -257,4 +223,5 @@ namespace Mewdeko.Services.Impl
             public int MemberCount { get; set; }
         }
     }
+
 }
