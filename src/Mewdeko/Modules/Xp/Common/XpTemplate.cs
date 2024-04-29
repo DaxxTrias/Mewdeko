4118c399de1c4e0129d26afab5cf846828f107a3
Sean <sean.daxxtrias@gmail.com>
2024-04-29 01:27:37 -0500

    more fixes

--- src/Mewdeko/Modules/Xp/Common/XpTemplate.cs
+++ src/Mewdeko/Modules/Xp/Common/XpTemplate.cs
@@ -1,260 +1,106 @@
-﻿using Newtonsoft.Json;
-using SixLabors.ImageSharp.PixelFormats;
-using Color = SixLabors.ImageSharp.Color;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Mewdeko.Modules.Xp.Common;
+namespace Mewdeko.Database.Models;
 
-public class XpTemplate
+[Table("Template")]
+public class Template : DbEntity
 {
-    [JsonProperty("output_size")]
-    public XpTemplatePos OutputSize { get; set; } = new()
-    {
-        X = 450, Y = 220
-    };
+    //General Stuff
+    public ulong GuildId { get; set; }
+    public int OutputSizeX { get; set; } = 797;
+    public int OutputSizeY { get; set; } = 279;
 
-    public XpTemplateUser User { get; set; } = new()
-    {
-        Name = new XpTemplateText
-        {
-            FontSize = 50,
-            Show = true,
-            Pos = new XpTemplatePos
-            {
-                X = 130, Y = 17
-            }
-        },
-        Icon = new XpTemplateIcon
-        {
-            Show = true,
-            Pos = new XpTemplatePos
-            {
-                X = 32, Y = 10
-            },
-            Size = new XpTemplatePos
-            {
-                X = 69, Y = 70
-            }
-        },
-        GuildLevel = new XpTemplateText
-        {
-            Show = true,
-            FontSize = 45,
-            Pos = new XpTemplatePos
-            {
-                X = 47, Y = 297
-            }
-        },
-        GlobalLevel = new XpTemplateText
-        {
-            Show = true,
-            FontSize = 45,
-            Pos = new XpTemplatePos
-            {
-                X = 47, Y = 149
-            }
-        },
-        GuildRank = new XpTemplateText
-        {
-            Show = true,
-            FontSize = 30,
-            Pos = new XpTemplatePos
-            {
-                X = 148, Y = 326
-            }
-        },
-        GlobalRank = new XpTemplateText
-        {
-            Show = true,
-            FontSize = 30,
-            Pos = new XpTemplatePos
-            {
-                X = 148, Y = 179
-            }
-        },
-        TimeOnLevel = new XpTemplateTimeOnLevel
-        {
-            Format = "{0}d{1}h{2}m",
-            Global = new XpTemplateText
-            {
-                FontSize = 20,
-                Show = true,
-                Pos = new XpTemplatePos
-                {
-                    X = 50, Y = 204
-                }
-            },
-            Guild = new XpTemplateText
-            {
-                FontSize = 20,
-                Show = true,
-                Pos = new XpTemplatePos
-                {
-                    X = 50, Y = 351
-                }
-            }
-        },
-        Xp = new XpTemplateXp
-        {
-            Bar = new XpTemplateXpBar
-            {
-                Show = true,
-                Global = new XpBar
-                {
-                    Direction = XpTemplateDirection.Right,
-                    Length = 450,
-                    Color = new Rgba32(0, 0, 0, 0.4f),
-                    PointA = new XpTemplatePos
-                    {
-                        X = 321, Y = 104
-                    },
-                    PointB = new XpTemplatePos
-                    {
-                        X = 286, Y = 235
-                    }
-                },
-                Guild = new XpBar
-                {
-                    Direction = XpTemplateDirection.Right,
-                    Length = 450,
-                    Color = new Rgba32(0, 0, 0, 0.4f),
-                    PointA = new XpTemplatePos
-                    {
-                        X = 282, Y = 248
-                    },
-                    PointB = new XpTemplatePos
-                    {
-                        X = 247, Y = 379
-                    }
-                }
-            },
-            Global = new XpTemplateText
-            {
-                Show = true,
-                FontSize = 50,
-                Pos = new XpTemplatePos
-                {
-                    X = 430, Y = 142
-                }
-            },
-            Guild = new XpTemplateText
-            {
-                Show = true,
-                FontSize = 50,
-                Pos = new XpTemplatePos
-                {
-                    X = 400, Y = 282
-                }
-            },
-            Awarded = new XpTemplateText
-            {
-                Show = true,
-                FontSize = 25,
-                Pos = new XpTemplatePos
-                {
-                    X = 445, Y = 347
-                }
-            }
-        }
-    };
+    // Time on Level
+    public string TimeOnLevelFormat { get; set; } = "{0}d{1}h{2}m";
+    public int TimeOnLevelX { get; set; } = 50;
+    public int TimeOnLevelY { get; set; } = 204;
+    public int TimeOnLevelFontSize { get; set; } = 20;
+    public string TimeOnLevelColor { get; set; } = "FF000000";
+    public bool ShowTimeOnLevel { get; set; } = true;
 
-    public XpTemplateClub Club { get; set; } = new()
-    {
-        Icon = new XpTemplateIcon
-        {
-            Show = true,
-            Pos = new XpTemplatePos
-            {
-                X = 722, Y = 25
-            },
-            Size = new XpTemplatePos
-            {
-                X = 45, Y = 45
-            }
-        },
-        Name = new XpTemplateText
-        {
-            FontSize = 35,
-            Pos = new XpTemplatePos
-            {
-                X = 650, Y = 49
-            },
-            Show = true
-        }
-    };
+    // Awarded
+    public int AwardedX { get; set; } = 445;
+    public int AwardedY { get; set; } = 347;
+    public int AwardedFontSize { get; set; } = 25;
+    public string AwardedColor { get; set; } = "ffffffff";
+    public bool ShowAwarded { get; set; }
+
+    // Navigation Properties
+    [ForeignKey("TemplateUserId")]
+    public TemplateUser TemplateUser { get; set; }
+
+    [ForeignKey("TemplateGuildId")]
+    public TemplateGuild TemplateGuild { get; set; }
+
+    [ForeignKey("TemplateClubId")]
+    public TemplateClub TemplateClub { get; set; }
+
+    [ForeignKey("TemplateBarId")]
+    public TemplateBar TemplateBar { get; set; }
 }
 
-public class XpTemplateIcon
+public class TemplateUser : DbEntity
 {
-    public bool Show { get; set; }
-    public XpTemplatePos Pos { get; set; }
-    public XpTemplatePos Size { get; set; }
+    // Username Text
+    public string TextColor { get; set; } = "FF000000";
+    public int FontSize { get; set; } = 50;
+    public int TextX { get; set; } = 120;
+    public int TextY { get; set; } = 70;
+    public bool ShowText { get; set; } = true;
+
+    // Icon
+    public int IconX { get; set; } = 27;
+    public int IconY { get; set; } = 24;
+    public int IconSizeX { get; set; } = 73;
+    public int IconSizeY { get; set; } = 74;
+    public bool ShowIcon { get; set; } = true;
 }
 
-public class XpTemplatePos
+public class TemplateGuild : DbEntity
 {
-    public int X { get; set; }
-    public int Y { get; set; }
+    // Guild Level
+    public string GuildLevelColor { get; set; } = "FF000000";
+    public int GuildLevelFontSize { get; set; } = 27;
+    public int GuildLevelX { get; set; } = 42;
+    public int GuildLevelY { get; set; } = 206;
+    public bool ShowGuildLevel { get; set; } = true;
+
+    // Guild Rank
+    public string GuildRankColor { get; set; } = "FF000000";
+    public int GuildRankFontSize { get; set; } = 25;
+    public int GuildRankX { get; set; } = 148;
+    public int GuildRankY { get; set; } = 211;
+    public bool ShowGuildRank { get; set; } = true;
 }
 
-public class XpTemplateUser
+public class TemplateClub : DbEntity
 {
-    public XpTemplateText Name { get; set; }
-    public XpTemplateIcon Icon { get; set; }
-    public XpTemplateText GlobalLevel { get; set; }
-    public XpTemplateText GuildLevel { get; set; }
-    public XpTemplateText GlobalRank { get; set; }
-    public XpTemplateText GuildRank { get; set; }
-    public XpTemplateTimeOnLevel TimeOnLevel { get; set; }
-    public XpTemplateXp Xp { get; set; }
+    // Club Icon
+    public int ClubIconX { get; set; } = 717;
+    public int ClubIconY { get; set; } = 37;
+    public int ClubIconSizeX { get; set; } = 49;
+    public int ClubIconSizeY { get; set; } = 49;
+    public bool ShowClubIcon { get; set; } = true;
+
+    // Club Name
+    public string ClubNameColor { get; set; } = "FF000000";
+    public int ClubNameFontSize { get; set; } = 32;
+    public int ClubNameX { get; set; } = 649;
+    public int ClubNameY { get; set; } = 50;
+    public bool ShowClubName { get; set; } = true;
 }
 
-public class XpTemplateTimeOnLevel
+public class TemplateBar : DbEntity
 {
-    public string Format { get; set; }
-    public XpTemplateText Global { get; set; }
-    public XpTemplateText Guild { get; set; }
-}
-
-public class XpTemplateClub
-{
-    public XpTemplateIcon Icon { get; set; }
-    public XpTemplateText Name { get; set; }
-}
-
-public class XpTemplateText
-{
-    [JsonConverter(typeof(XpRgba32Converter))]
-    public Rgba32 Color { get; set; } = SixLabors.ImageSharp.Color.White;
-
-    public bool Show { get; set; }
-    public int FontSize { get; set; }
-    public XpTemplatePos Pos { get; set; }
-}
-
-public class XpTemplateXp
-{
-    public XpTemplateXpBar Bar { get; set; }
-    public XpTemplateText Global { get; set; }
-    public XpTemplateText Guild { get; set; }
-    public XpTemplateText Awarded { get; set; }
-}
-
-public class XpTemplateXpBar
-{
-    public bool Show { get; set; }
-    public XpBar Global { get; set; }
-    public XpBar Guild { get; set; }
-}
-
-public class XpBar
-{
-    [JsonConverter(typeof(XpRgba32Converter))]
-    public Rgba32 Color { get; set; }
-
-    public XpTemplatePos PointA { get; set; }
-    public XpTemplatePos PointB { get; set; }
-    public int Length { get; set; }
-    public XpTemplateDirection Direction { get; set; }
+    public string BarColor { get; set; } = "FF000000";
+    public int BarPointAx { get; set; } = 319;
+    public int BarPointAy { get; set; } = 119;
+    public int BarPointBx { get; set; } = 284;
+    public int BarPointBy { get; set; } = 250;
+    public int BarLength { get; set; } = 452;
+    public byte BarTransparency { get; set; } = 90;
+    public XpTemplateDirection BarDirection { get; set; } = XpTemplateDirection.Right;
+    public bool ShowBar { get; set; } = true;
 }
 
 public enum XpTemplateDirection
@@ -263,13 +109,4 @@ public enum XpTemplateDirection
     Down,
     Left,
     Right
-}
-
-public class XpRgba32Converter : JsonConverter<Rgba32>
-{
-    public override Rgba32 ReadJson(JsonReader reader, Type objectType, Rgba32 existingValue, bool hasExistingValue,
-        JsonSerializer serializer) =>
-        Color.ParseHex(reader.Value.ToString());
-
-    public override void WriteJson(JsonWriter writer, Rgba32 value, JsonSerializer serializer) => writer.WriteValue(value.ToHex().ToLowerInvariant());
 }
