7e084450026ac10d19f9a4ffa7c321197df10df6
Sean <sean.daxxtrias@gmail.com>
2024-06-24 03:02:02 -0500

    compiles & runs, but not honoring cmd prefix properly

--- src/Mewdeko/Modules/Administration/Services/VcRoleService.cs
+++ src/Mewdeko/Modules/Administration/Services/VcRoleService.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Concurrent;
+using System.Collections.Concurrent;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -102,7 +102,7 @@ public class VcRoleService : INService
         var infos = new NonBlocking.ConcurrentDictionary<ulong, IRole>();
         var missingRoles = new List<VcRoleInfo>();
         VcRoles.AddOrUpdate(gconf.GuildId, infos, delegate { return infos; });
-        foreach (var ri in gconf.VcRoleInfos)
+        foreach (var ri in gconf.VcRoleInfos) //todo: can nullref on this? why
         {
             var role = g.GetRole(ri.RoleId);
             if (role == null)
