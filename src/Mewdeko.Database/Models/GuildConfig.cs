d9e1f83db43b2741cdcd5e417a5d3daa0bf7c311
Sean <sean.daxxtrias@gmail.com>
2024-06-24 07:05:02 -0500

    models port

--- src/Mewdeko.Database/Models/GuildConfig.cs
+++ src/Mewdeko.Database/Models/GuildConfig.cs
@@ -1,7 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using Mewdeko.Database.Common;
-
-#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
 
 // ReSharper disable InconsistentNaming
 
@@ -24,13 +22,6 @@ public class GuildConfig : DbEntity
     public string CurrencyEmoji { get; set; } = "💰";
     public int RewardAmount { get; set; } = 200;
     public int RewardTimeoutSeconds { get; set; } = 86400;
-
-    public string GiveawayBanner { get; set; } = "";
-    public string GiveawayEmbedColor { get; set; } = "";
-    public string GiveawayWinEmbedColor { get; set; } = "";
-    public long DmOnGiveawayWin { get; set; } = 0;
-    public string GiveawayEndMessage { get; set; } = "";
-    public ulong GiveawayPingRole { get; set; } = 0;
 
     // Starboard
     public long StarboardAllowBots { get; set; } = 1;
