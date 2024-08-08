b59478ce7c24e30adb4c39cae15505dbbddee69f
sylveondeko <crimsonslayer76@gmail.com>
2024-08-08 14:07:07 -0400

    e
    (cherry picked from commit dd6c008d4338658f780e64ea5cd339c7cfa1a45c)

--- src/Mewdeko/Program.cs
+++ src/Mewdeko/Program.cs
@@ -179,7 +179,6 @@ public class Program
 
         var auth = services.AddAuthentication(options =>
         {
-            options.DefaultScheme = credentials.SkipApiKey ? "Default" : "ApiKey";
             options.AddScheme<AuthHandler>(AuthHandler.SchemeName, AuthHandler.SchemeName);
         });
 
