5c1458c59d8bc951ea8590b415a16db2307a3643
sylveondeko <crimsonslayer76@gmail.com>
2024-08-13 23:43:02 -0400

    add preliminary ticket stuff, add message counting and message reqs for giveaways
    (cherry picked from commit fe22ea0d143e6f5a25f2f18bbd95114121567a1a)

--- src/Mewdeko/Modules/OwnerOnly/Services/OwnerOnlyService.cs
+++ src/Mewdeko/Modules/OwnerOnly/Services/OwnerOnlyService.cs
@@ -55,7 +55,7 @@ public class OwnerOnlyService : ILateExecutor, IReadyExecutor, INService
     /// </summary>
     /// <param name="client">The Discord client used for interacting with the Discord API.</param>
     /// <param name="cmdHandler">Handles command processing and execution.</param>
-    /// <param name="db">Provides access to the database for data persistence.</param>
+    /// <param name="dbProvider">Provides access to the database for data persistence.</param>
     /// <param name="strings">Provides access to localized bot strings.</param>
     /// <param name="creds">Contains the bot's credentials and configuration.</param>
     /// <param name="cache">Provides caching functionalities.</param>
@@ -596,7 +596,9 @@ private async Task StreamResponseAndUpdateEmbedAsync(string apiKey, string model
             if (!((DateTimeOffset.UtcNow - lastUpdate).TotalSeconds >= 1)) continue;
             lastUpdate = DateTimeOffset.UtcNow;
             var embeds = BuildEmbeds(responseBuilder.ToString(), author,
-                chatResponse.Usage is not null ? toUpdate.actualItem.GptTokensUsed + chatResponse.Usage.TotalTokens : toUpdate.actualItem.GptTokensUsed);
+                chatResponse.Usage is not null
+                    ? toUpdate.actualItem.GptTokensUsed + chatResponse.Usage.TotalTokens
+                    : toUpdate.actualItem.GptTokensUsed);
             if (chatResponse.Usage is not null)
                 totalTokens += chatResponse.Usage.TotalTokens;
             await loadingMsg.ModifyAsync(m => m.Embeds = embeds.ToArray());
