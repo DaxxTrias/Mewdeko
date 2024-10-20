672bb31870f11495e8441decd04421113f018335
Sean <sean.daxxtrias@gmail.com>
2024-10-20 17:07:58 -0500

    porting changes from psqldeko as of commit 5ae01f113518a40bad81c866caad8a8966b41c4c

--- src/Mewdeko.Database/Models/GlobalBanConfig.cs
+++ src/Mewdeko.Database/Models/GlobalBanConfig.cs
@@ -4,12 +4,12 @@ public class GlobalBanConfig : DbEntity
 {
     public ulong GuildId { get; set; }
     public ulong GlobalBanLogChannel { get; set; }
-    public GBActionType Action { get; set; }
+    public GbActionType Action { get; set; }
     public long UseRecommendedAction { get; set; }
     public int Duration { get; set; }
 }
 
-public enum GBActionType
+public enum GbActionType
 {
     Kick,
     Ban,
