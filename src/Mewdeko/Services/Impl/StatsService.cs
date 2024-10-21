a66e351b3aded3edd744910724a03b3951585537
Sean <sean.daxxtrias@gmail.com>
2024-10-20 20:19:21 -0500

    update mewdeko to 7.4.0 (close to parity with psqldeko-34 as of dec 25 2023)

--- src/Mewdeko/Services/Impl/StatsService.cs
+++ src/Mewdeko/Services/Impl/StatsService.cs
@@ -19,7 +19,7 @@ public class StatsService : IStatsService, IReadyExecutor
     public readonly ICoordinator Coord;
     private readonly IDataCache cache;
     private readonly HttpClient http;
-    public const string BotVersion = "7.3.0";
+    public const string BotVersion = "7.4.0";
 
     private readonly DateTime started;
 
