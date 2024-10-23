3e95133a34f55945183eca82f245d2b5495c8b97
Sean <sean.daxxtrias@gmail.com>
2024-10-23 14:27:54 -0500

    bump frog to 7.4.1 parity with psqldeko-40

--- src/Mewdeko/Services/Impl/StatsService.cs
+++ src/Mewdeko/Services/Impl/StatsService.cs
@@ -19,7 +19,7 @@ public class StatsService : IStatsService, IReadyExecutor
     public readonly ICoordinator Coord;
     private readonly IDataCache cache;
     private readonly HttpClient http;
-    public const string BotVersion = "7.4.0";
+    public const string BotVersion = "7.4.1";
 
     private readonly DateTime started;
 
