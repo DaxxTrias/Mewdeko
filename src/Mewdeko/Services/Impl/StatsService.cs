f2865c14e19f9c96737b2c3614f57a7e1c998a40
Sean <sean.daxxtrias@gmail.com>
2024-11-08 11:29:15 -0600

    bump to 7.5.2 ; at parity with psqldeko-93 as well as specific bits from new upstreams good place to checkpoint before setting up psql env

--- src/Mewdeko/Services/Impl/StatsService.cs
+++ src/Mewdeko/Services/Impl/StatsService.cs
@@ -26,7 +26,7 @@ namespace Mewdeko.Services.Impl
         /// <summary>
         /// The version of the bot. I should make this set from commits somehow idk
         /// </summary>
-        public const string BotVersion = "7.5.1";
+        public const string BotVersion = "7.5.2";
 
         private readonly DateTime started;
         private PeriodicTimer statcordTimer;
