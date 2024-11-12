eda7c5d9d306bc4cf65c00e4a944c5f1c6a1f6b2
Sean <sean.daxxtrias@gmail.com>
2024-11-11 23:08:21 -0600

    - bump to 7.6.7 - music now has a shuffle-queue mode

--- src/Mewdeko/Services/Impl/StatsService.cs
+++ src/Mewdeko/Services/Impl/StatsService.cs
@@ -20,7 +20,7 @@ public class StatsService : IStatsService, IDisposable
     /// <summary>
     ///     The version of the bot. I should make this set from commits somehow idk
     /// </summary>
-    public const string BotVersion = "7.6.6";
+    public const string BotVersion = "7.6.7";
 
     private readonly IDataCache cache;
     private readonly DiscordShardedClient client;
