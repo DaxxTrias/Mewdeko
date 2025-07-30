d73302b794462581fd71aaa40124e3f9cbe87f02
Sean <sean.daxxtrias@gmail.com>
2025-07-30 13:23:00 -0500

    fully upstreamed, bump ver

--- src/Mewdeko/Services/Impl/StatsService.cs
+++ src/Mewdeko/Services/Impl/StatsService.cs
@@ -18,7 +18,7 @@ public class StatsService : IStatsService, IDisposable
     /// <summary>
     ///     The version of the bot. I should make this set from commits somehow idk
     /// </summary>
-    public const string BotVersion = "7.8.3";
+    public const string BotVersion = "7.8.4";
 
     private readonly IDataCache cache;
     private readonly DiscordShardedClient client;
